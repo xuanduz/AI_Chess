44d218d85ff8a4cf5dc4754314811535cc355888
congnguyen27 <69128373+congnguyen27@users.noreply.github.com>
2022-11-09 22:30:34 +0700

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,119 @@
+var board,
+  game = new Chess();
+
+
+var onDragStart = function (source, piece, position, orientation) {
+  if (
+    game.in_checkmate() === true ||
+    game.in_draw() === true ||
+    piece.search(/^b/) !== -1
+  ) {
+    return true;
+  }
+};
+
+
+var renderMoveHistory = function (moves) {
+  var historyElement = $("#move-history").empty();
+  historyElement.empty();
+  for (var i = 0; i < moves.length; i = i + 2) {
+    historyElement.append(
+      "<span>" +
+        moves[i] +
+        " " +
+        (moves[i + 1] ? moves[i + 1] : " ") +
+        "</span><br>"
+    );
+  }
+  historyElement.scrollTop(historyElement[0].scrollHeight);
+};
+
+var onDrop = function (source, target) {
+  var move = game.move({
+    from: source,
+    to: target,
+    promotion: "q",
+  });
+
+  removeGreySquares();
+  if (move === null) {
+    return "snapback";
+  }
+
+  renderMoveHistory(game.history());
+
+  if (game.game_over()) {
+    alert("Game over");
+  }
+};
+
+var onSnapEnd = function () {
+  board.position(game.fen());
+};
+
+var onMouseoverSquare = function (square, piece) {
+  var moves = game.moves({
+    square: square,
+    verbose: true,
+  });
+
+  if (moves.length === 0) return;
+  if ($("#startBtn").is(":disabled")) {
+    greySquare(square);
+
+    for (var i = 0; i < moves.length; i++) {
+      greySquare(moves[i].to);
+    }
+  }
+  
+};
+
+var onMouseoutSquare = function (square, piece) {
+  removeGreySquares();
+};
+
+var removeGreySquares = function () {
+  $("#board .square-55d63").css("background", "");
+};
+
+var greySquare = function (square) {
+  var squareEl = $("#board .square-" + square);
+
+  var background = "#a9a9a9";
+  if (squareEl.hasClass("black-3c85d") === true) {
+    background = "#696969";
+  }
+
+  squareEl.css("background", background);
+};
+
+var cfg = {
+  draggable: true,
+  position: "clear",
+  onDragStart: onDragStart,
+  onDrop: onDrop,
+  onMouseoutSquare: onMouseoutSquare,
+  onMouseoverSquare: onMouseoverSquare,
+  onSnapEnd: onSnapEnd,
+  dropOffBoard: "trash",
+  sparePieces: true,
+};
+board = ChessBoard("board", cfg);
+
+$("#startBtn").click(function () {
+  board.start();
+
+  $("#startBtn").prop("disabled", true);
+});
+
+$("#clearBtn").click(function () {
+  board.clear();
+  game = new Chess();
+  $("#position-count").text("");
+  $("#time").text("");
+  $("#positions-per-s").text("");
+  $("#move-history").empty();
+
+  $("#startBtn").prop("disabled", false);
+  $("#search-depth").prop("disabled", false);
+});
